b264d2b755656672332fb9b84f5d0cc1e0eb7943
jyef <tubudakusan@gmail.com>
2020-02-08 13:24:55 +0000

    redirect to original domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,19 @@ class ApplicationController < ActionController::Base
   
   include SessionsHelper
   
+  #herokuapp.comから独自ドメインへリダイレクト
+  before_filter :ensure_domain
+  FQDN = '.ohanami.work'
+ 
+  # redirect correct server from herokuapp domain for SEO
+  def ensure_domain
+   return unless /\.herokuapp.com/ =~ request.host
+   
+   # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める 
+   port = ":#{request.port}" unless [80, 443].include?(request.port)
+   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
+  
   def require_user_logged_in
     unless logged_in?
       redirect_to root_url
