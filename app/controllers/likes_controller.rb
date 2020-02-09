481ff99bfca076831f4394628ba31c71e46417f4
jyef <tubudakusan@gmail.com>
2020-02-09 05:57:50 +0000

    improve like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,13 @@ class LikesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
+    @heart = params[:heart]
     @review = Review.find(params[:review_id])
     current_user.favorite(@review)
   end
 
   def destroy
+    @heart = params[:heart]
     @review = Review.find(params[:review_id])
     current_user.unfavorite(@review)
   end
